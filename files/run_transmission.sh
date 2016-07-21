0b9df2a63b1fe7642b99af3a39f8cc758b43fc7e
ytf4425 <ytf4425@163.com>
2016-07-21 12:05:22 +0800

    test

--- files/run_transmission.sh
+++ files/run_transmission.sh
@@ -6,8 +6,6 @@ CONFIG_DIR=/etc/transmission-daemon
 SETTINGS=$CONFIG_DIR/settings.json
 TRANSMISSION=/usr/bin/transmission-daemon
 
-unset TRANSMISION_ADMIN_PASS
-
 exec $TRANSMISSION -f --no-portmap $EXTRA_FLAGS --config-dir $CONFIG_DIR 
 
 
